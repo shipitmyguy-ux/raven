218cb211d14ee7d5443f75814d27e2f1bc680ac2
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-17 20:08:09 -0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RAVEN_CONFIG = {
+  gatewayUrl: "https://script.google.com/macros/s/AKfycbztpeEuCsgzKX6QUqxxxuthXI0jr6jRgX5CAZiW-_RL6eTw9AlthcKJcqdd7brOHFlhhQ/exec",
+  chromeExtensionArchiveName: "raven-capture-extension.zip"
+};
+window.JOBTRACK_CONFIG = window.RAVEN_CONFIG;
