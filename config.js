2361f07d1946b39b00a0bbd2e191bc55dd272414
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-17 20:46:13 -0700

    Configure Raven background search API

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 window.RAVEN_CONFIG = {
   gatewayUrl: "https://script.google.com/macros/s/AKfycbztpeEuCsgzKX6QUqxxxuthXI0jr6jRgX5CAZiW-_RL6eTw9AlthcKJcqdd7brOHFlhhQ/exec",
+  searchApiUrl: "https://umvmilulnqnmeqvfoxxc.supabase.co/functions/v1/raven-search",
+  searchAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdm1pbHVsbnFubWVxdmZveHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2OTExMDMsImV4cCI6MjEwNTI2NzEwM30.M73kpySfNuWuOTNF8QNPwdtS58Ezpj0lpM0ljKWqRJU",
   chromeExtensionArchiveName: "raven-capture-extension.zip"
 };
 window.JOBTRACK_CONFIG = window.RAVEN_CONFIG;
