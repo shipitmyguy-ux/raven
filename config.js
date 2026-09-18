96f86c2b5bfe7c5e0b73983c0690ee16160e3ca0
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-18 01:03:24 -0600

    Point Raven API client at isolated search backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.RAVEN_CONFIG = {
   gatewayUrl: "https://script.google.com/macros/s/AKfycbztpeEuCsgzKX6QUqxxxuthXI0jr6jRgX5CAZiW-_RL6eTw9AlthcKJcqdd7brOHFlhhQ/exec",
-  searchApiUrl: "https://umvmilulnqnmeqvfoxxc.supabase.co/functions/v1/raven-search",
+  searchApiUrl: "https://umvmilulnqnmeqvfoxxc.supabase.co/functions/v1/raven-search-v2",
   searchAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdm1pbHVsbnFubWVxdmZveHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2OTExMDMsImV4cCI6MjEwNTI2NzEwM30.M73kpySfNuWuOTNF8QNPwdtS58Ezpj0lpM0ljKWqRJU",
   chromeExtensionArchiveName: "raven-capture-extension.zip"
 };
